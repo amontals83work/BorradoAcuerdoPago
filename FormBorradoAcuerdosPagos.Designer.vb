5153f731390eb21532379938af7bec62aecad9b7
amontals83work <151736333+amontals83work@users.noreply.github.com>
2024-02-22 12:13:38 +0100

    Add files via upload

---
+++ FormBorradoAcuerdosPagos.Designer.vb
@@ -0,0 +1,221 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormBorradoAcuerdosPagos
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnBorrar = New System.Windows.Forms.Button()
+        Me.btnFichero = New System.Windows.Forms.Button()
+        Me.txtFichero = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnInstrucciones = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(21, 94)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 13)
+        Me.Label3.TabIndex = 34
+        Me.Label3.Text = "Fichero"
+        '
+        'Label4
+        '
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(64, 27)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(251, 48)
+        Me.Label4.TabIndex = 33
+        Me.Label4.Text = "BORRADO ACUERDOS DE PAGOS"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnBorrar
+        '
+        Me.btnBorrar.Location = New System.Drawing.Point(252, 126)
+        Me.btnBorrar.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnBorrar.Name = "btnBorrar"
+        Me.btnBorrar.Size = New System.Drawing.Size(100, 28)
+        Me.btnBorrar.TabIndex = 32
+        Me.btnBorrar.Text = "Borrar"
+        Me.btnBorrar.UseVisualStyleBackColor = True
+        '
+        'btnFichero
+        '
+        Me.btnFichero.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnFichero.Location = New System.Drawing.Point(313, 86)
+        Me.btnFichero.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnFichero.Name = "btnFichero"
+        Me.btnFichero.Size = New System.Drawing.Size(39, 28)
+        Me.btnFichero.TabIndex = 31
+        Me.btnFichero.Text = "..."
+        Me.btnFichero.UseVisualStyleBackColor = True
+        '
+        'txtFichero
+        '
+        Me.txtFichero.Location = New System.Drawing.Point(71, 91)
+        Me.txtFichero.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtFichero.Name = "txtFichero"
+        Me.txtFichero.Size = New System.Drawing.Size(234, 20)
+        Me.txtFichero.TabIndex = 30
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Label8)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.Label9)
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(389, 13)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(175, 148)
+        Me.Panel1.TabIndex = 43
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(-3, 104)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(0, 15)
+        Me.Label8.TabIndex = 8
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(106, 66)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 15)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "FechaPP"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(101, 44)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(71, 15)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "2ª Columna"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(3, 102)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(150, 15)
+        Me.Label9.TabIndex = 5
+        Me.Label9.Text = "Solo para Arborknot y TDX"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(6, 66)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(69, 15)
+        Me.Label7.TabIndex = 3
+        Me.Label7.Text = "Expediente"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(4, 44)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(71, 15)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "1ª Columna"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(14, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(149, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ESTRUCTURA FICHERO"
+        '
+        'btnInstrucciones
+        '
+        Me.btnInstrucciones.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.btnInstrucciones.FlatAppearance.BorderSize = 0
+        Me.btnInstrucciones.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnInstrucciones.Location = New System.Drawing.Point(24, 127)
+        Me.btnInstrucciones.Name = "btnInstrucciones"
+        Me.btnInstrucciones.Size = New System.Drawing.Size(79, 27)
+        Me.btnInstrucciones.TabIndex = 44
+        Me.btnInstrucciones.Text = "Instrucciones"
+        Me.btnInstrucciones.UseVisualStyleBackColor = False
+        '
+        'FormBorradoAcuerdosPagos
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(584, 181)
+        Me.Controls.Add(Me.btnInstrucciones)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnBorrar)
+        Me.Controls.Add(Me.btnFichero)
+        Me.Controls.Add(Me.txtFichero)
+        Me.Name = "FormBorradoAcuerdosPagos"
+        Me.Text = "FormBorradoAcuerdosPagos"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnBorrar As Button
+    Friend WithEvents btnFichero As Button
+    Friend WithEvents txtFichero As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnInstrucciones As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label8 As Label
+End Class
---
+++ FormBorradoAcuerdosPagos.vb
@@ -0,0 +1,210 @@
+﻿Imports System.Data
+Imports System.Threading
+Imports Microsoft.Office.Interop
+Imports System.Text.RegularExpressions
+
+Public Class FormBorradoAcuerdosPagos
+#Region "BarraTareasMC"
+    Implements TaskbarWindow
+    Private mTaskbarButton As ToolStripItem
+    Public Property TaskbarButton() As System.Windows.Forms.ToolStripItem Implements TaskbarWindow.TaskbarButton
+        Get
+            Return mTaskbarButton
+        End Get
+        Set(ByVal value As System.Windows.Forms.ToolStripItem)
+            mTaskbarButton = value
+        End Set
+    End Property
+#End Region
+
+    Private mc As New MCCommand
+    Private mcAux As New MCCommand
+    Private openFileDialog As New OpenFileDialog
+
+    Private Sub FormBorradoAcuerdosPagos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Width = 395
+    End Sub
+
+    Private Sub btnFichero_Click(sender As Object, e As EventArgs) Handles btnFichero.Click
+        If OpenFileDialog.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            txtFichero.Text = OpenFileDialog.FileName
+        Else
+            MessageBox.Show("Debe seleccionar un fichero para cargar los expedientes")
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub btnBorrar_Click(sender As Object, e As EventArgs) Handles btnBorrar.Click
+        BorrarAP()
+    End Sub
+
+    Private Sub BorrarAP()
+
+        Dim MyConnection As System.Data.OleDb.OleDbConnection = New System.Data.OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + txtFichero.Text.Trim + ";Extended Properties=""Excel 12.0 Xml;HDR=YES;IMEX=1"";")
+        Dim hoja As String = nhoja()
+        Dim MyCommand As System.Data.OleDb.OleDbDataAdapter = New System.Data.OleDb.OleDbDataAdapter("select * from [" + hoja + "$]", MyConnection)
+
+        Dim ds As System.Data.DataSet = New System.Data.DataSet()
+        MyCommand.Fill(ds)
+        MyConnection.Close()
+
+        Dim dt As DataTable = ds.Tables(0)
+
+        If Check_Excel(dt) Then
+            Dim expediente As String = "", observacion As String = "", formatObser As String = "", fechaIPP As String = "", fechaPP As String = "", fechaChecked As String = "", count As Integer = 0, numExp As Integer = 0, numPP As Integer = 0, idExpediente As Object = DBNull.Value
+            Dim aExpRep As New List(Of String)()
+            Dim aExpNoPP As New List(Of String)()
+
+            Dim messageThread As New Thread(AddressOf MuestraMensaje)
+            messageThread.Start()
+
+            For Each fila As DataRow In dt.Rows
+                expediente = fila(0).ToString
+                fechaIPP = fila(1).ToString
+                observacion = ""
+                fechaPP = ""
+
+                'COMPROBAMOS SI HAY COINCIDENCIA ENTRE Expediente Y RefCliente
+                If Not String.IsNullOrEmpty(expediente) Then
+                    If Not Integer.TryParse(expediente, Nothing) Then
+                        mc.CommandText = "SELECT COUNT(*) FROM Expedientes WHERE cast(Expediente as varchar)='" & expediente.ToString() & "' OR RefCliente='" & expediente.ToString() & "'"
+                    Else
+                        mc.CommandText = "SELECT COUNT(*) FROM Expedientes WHERE Expediente=" & expediente.ToString() & " OR RefCliente='" & expediente.ToString() & "'"
+                    End If
+                    numExp = mc.ExecuteScalar()
+                    If numExp <> 1 Then
+                        idExpediente = DBNull.Value
+                        aExpRep.Add(expediente.ToString())
+                    Else
+                        'SI HAY SOLO UNA COINCIDENCIA BUSCAMOS SU idExpediente
+                        If Not Integer.TryParse(expediente, Nothing) Then
+                            mc.CommandText = "SELECT idExpediente FROM Expedientes WHERE cast(Expediente as varchar)='" & expediente.ToString() & "' OR RefCliente='" & expediente.ToString() & "'"
+                        Else
+                            mc.CommandText = "SELECT idExpediente FROM Expedientes WHERE Expediente='" & expediente.ToString() & "' OR RefCliente='" & expediente.ToString() & "'"
+                        End If
+                        idExpediente = mc.ExecuteScalar()
+
+                        Dim result As Integer = If(idExpediente IsNot DBNull.Value, Convert.ToInt32(idExpediente), 0)
+                        'SI HAY idExpediente COMPROBAMOS SI TIENE UNA ACUERDO DE PAGO
+                        If result <> 0 Then
+                            mc.CommandText = "SELECT COUNT(*) FROM Acciones WHERE idExpediente=" & idExpediente.ToString() & " AND idTipoNota=1"
+                            numPP = mc.ExecuteScalar()
+
+                            If numPP = 0 Then
+                                aExpNoPP.Add(expediente.ToString())
+                            Else
+                                mc.CommandText = "SELECT Observaciones FROM Acciones WHERE idExpediente=" & idExpediente.ToString() & " AND idTipoNota=1"
+                                fechaChecked = Check_Fecha(fechaIPP, expediente)
+                                If Not String.IsNullOrEmpty(fechaChecked) Then
+                                    observacion = "INCUMPLIMIENTO: " & fechaChecked.ToString() & " // " & mc.ExecuteScalar().Replace("'", "")
+                                Else
+                                    observacion = "INCUMPLIMIENTO // " & mc.ExecuteScalar().Replace("'", "")
+                                End If
+                                mc.CommandText = "SELECT Fecha FROM Acciones WHERE idExpediente=" & idExpediente.ToString() & " AND idTipoNota=1"
+                                fechaPP = mc.ExecuteScalar()
+
+                                mc.CommandText = "INSERT INTO Acciones (idExpediente, idTipoNota, Fecha, Observaciones, Usuario, Hermes, Borrado, Activa) VALUES ('" & idExpediente.ToString() & "',749,'" & fechaPP.ToString() & "','" & observacion.ToString() & "','Automarcador',0,0,1)"
+                                mc.ExecuteNonQuery()
+
+                                mc.CommandText = "UPDATE Acciones SET borrado=1 WHERE idExpediente=" & idExpediente.ToString() & " AND idTipoNota=1"
+                                mc.ExecuteNonQuery()
+
+                                mc.CommandText = "UPDATE Expedientes SET AcuerdoPago=0 WHERE idExpediente=" & idExpediente.ToString()
+                                mc.ExecuteNonQuery()
+                                count += 1
+                            End If
+                        End If
+                    End If
+                End If
+            Next
+
+            MessageBox.Show("Se han borrado " + count.ToString + " Acuerdos de Pago.")
+
+            If aExpRep.Count > 0 Then
+                Dim encabezadoRep As String = "Expedientes y RefClientes que coinciden: " & Environment.NewLine
+                For Each repe As String In aExpRep
+                    encabezadoRep &= repe & Environment.NewLine
+                Next
+                MessageBox.Show(encabezadoRep)
+            End If
+            If aExpNoPP.Count > 0 Then
+                Dim encabezadoNoPP As String = "Expedientes sin Acuerdos de Pagos: " & Environment.NewLine
+                For Each repe As String In aExpNoPP
+                    encabezadoNoPP &= repe & Environment.NewLine
+                Next
+                MessageBox.Show(encabezadoNoPP)
+            End If
+        Else
+            MessageBox.Show("Después de la modificación vuelva a cargar el archivo con otro nombre")
+        End If
+    End Sub
+
+    Private Function Check_Fecha(fecha As String, expediente As String) As String
+        If Not String.IsNullOrEmpty(fecha) Then
+            Dim formatFecha As String = Convert.ToDateTime(fecha).ToString("dd/MM/yyyy")
+            If formatFecha.Length = 10 Then
+                Dim sRegex As String = "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/[0-9]{4}$"
+                Dim result As Match = Regex.Match(formatFecha, sRegex)
+                If result.Success Then
+                    Return formatFecha
+                Else
+                    MessageBox.Show("Comprobar el formato de celda 'FechaPP' es DD/MM/YYYY")
+                    Return Nothing
+                End If
+            Else
+                MessageBox.Show("Comprobar el formato de celda 'FechaPP' es DD/MM/YYYY")
+                Return Nothing
+            End If
+        Else
+            MessageBox.Show("No existe fecha en el EXPEDIENTE: " & expediente)
+        End If
+    End Function
+
+    Private Function Check_Excel(dt As DataTable) As Boolean
+        Dim expediente As String = If(dt.Rows(0).Table.Columns.Contains("Expediente"), dt.Rows(0)("Expediente")?.ToString(), Nothing)
+        Dim fecha As String = If(dt.Rows(0).Table.Columns.Contains("FechaPP"), dt.Rows(0)("FechaPP")?.ToString().ToLower(), Nothing)
+
+        If Not String.IsNullOrEmpty(expediente) Then
+            If Not String.IsNullOrEmpty(fecha) Then
+                Check_Fecha(fecha, expediente)
+                Return True
+            Else
+                MessageBox.Show("Comprobar si la segunda columna esta nombrada como 'FechaPP'")
+                Return False
+            End If
+        Else
+            MessageBox.Show("Comprobar si la primera columna esta nombrada como 'Expediente'")
+            Return False
+        End If
+    End Function
+
+    Private Shared Sub MuestraMensaje()
+        MessageBox.Show("Borrando los Acuerdos de Pago." + vbCrLf + "Este proceso puede tardar varios minutos y ralentizar el programa DespachoMC." + vbCrLf + "Al finalizar aparecerá un aviso.")
+    End Sub
+
+    Private Function nhoja() As String
+        Dim hoja As String
+        Dim xlsApp = New Excel.Application()
+        xlsApp.Workbooks.Open(txtFichero.Text.Trim)
+
+        hoja = xlsApp.Sheets(1).Name
+
+        xlsApp.DisplayAlerts = False
+        xlsApp.Workbooks.Close()
+        xlsApp.DisplayAlerts = True
+
+        xlsApp.Quit()
+        xlsApp = Nothing
+
+        Return hoja
+    End Function
+
+    Private Sub btnInstrucciones_Click(sender As Object, e As EventArgs) Handles btnInstrucciones.Click
+        If Me.Width = 395 Then
+            Me.Width = 600
+        Else
+            Me.Width = 395
+        End If
+    End Sub
+
+End Class
